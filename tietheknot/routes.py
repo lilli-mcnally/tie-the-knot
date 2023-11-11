4543f81424be4f9c4e0c84ac928075b037a55617
Lilli McNally <Lilli.lawlor@hotmail.co.uk>
2023-11-11 22:47:46 +0000

    Add Create and View functionality for Checklist

--- tietheknot/routes.py
+++ tietheknot/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, request, redirect, url_for
 from tietheknot import app, db
 from tietheknot.models import Checklist, Table, Guest
 
@@ -14,10 +14,21 @@ def dashboard():
 
 @app.route("/checklist")
 def checklist():
-    return render_template("checklist.html")
+    checklist_items = list(Checklist.query.order_by(Checklist.checklist_date).all())
+    return render_template("checklist.html", checklist_items=checklist_items)
 
 @app.route("/add_checklist_item", methods=["GET", "POST"])
 def add_checklist_item():
+    if request.method == "POST":
+            checklist_item = Checklist(
+                checklist_name=request.form.get("checklist_name"),
+                checklist_notes=request.form.get("checklist_notes"),
+                checklist_date=request.form.get("checklist_date"),
+                checklist_payment=bool(True if request.form.get("checklist_payment") else False)
+            )
+            db.session.add(checklist_item)
+            db.session.commit()
+            return redirect(url_for("checklist"))
     return render_template("add_checklist_item.html")
 
 
