3da526e52c503f0cc283c9a493c0d7a4cb414308
Lilli McNally <Lilli.lawlor@hotmail.co.uk>
2023-11-12 18:14:20 +0000

    Add edit function to checklist items

--- tietheknot/routes.py
+++ tietheknot/routes.py
@@ -22,7 +22,7 @@ def add_checklist_item():
     if request.method == "POST":
             checklist_item = Checklist(
                 checklist_name=request.form.get("checklist_name"),
-                checklist_notes=request.form.get("checklist_notes"),
+                checklist_notes=request.form.get("checklist_notes"),    
                 checklist_date=request.form.get("checklist_date"),
                 checklist_payment=bool(True if request.form.get("checklist_payment") else False)
             )
@@ -31,6 +31,17 @@ def add_checklist_item():
             return redirect(url_for("checklist"))
     return render_template("add_checklist_item.html")
 
+@app.route("/edit_checklist_item/<int:checklist_item_id>", methods=["GET", "POST"])
+def edit_checklist_item(checklist_item_id):
+    checklist_item = Checklist.query.get_or_404(checklist_item_id)
+    if request.method == "POST":
+        checklist_item.checklist_name = request.form.get("checklist_name")
+        checklist_item.checklist_notes = request.form.get("checklist_notes")
+        checklist_item.checklist_date = request.form.get("checklist_date")
+        checklist_item.checklist_payment = bool(True if request.form.get("checklist_payment") else False)
+        db.session.commit()
+        return redirect(url_for("checklist"))
+    return render_template("edit_checklist_item.html", checklist_item=checklist_item)
 
 @app.route("/guests")
 def guests():
--- tietheknot/static/js/script.js
+++ tietheknot/static/js/script.js
@@ -2,3 +2,8 @@ document.addEventListener('DOMContentLoaded', function () {
     var sideNavbar = document.querySelectorAll('.sidenav');
     M.Sidenav.init(sideNavbar);
 });
+
+document.addEventListener('DOMContentLoaded', function () {
+    var elems = document.querySelectorAll('.modal');
+    M.Modal.init(elems);
+});
