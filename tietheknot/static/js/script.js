813b10868c640793a35f00ac54a19988d59436fb
Lilli McNally <Lilli.lawlor@hotmail.co.uk>
2023-11-16 18:38:38 +0000

    Add dropdown to Add Guest page

--- tietheknot/static/js/script.js
+++ tietheknot/static/js/script.js
@@ -7,3 +7,19 @@ document.addEventListener('DOMContentLoaded', function () {
     var elems = document.querySelectorAll('.modal');
     M.Modal.init(elems);
 });
+
+document.addEventListener('DOMContentLoaded', function () {
+    var elems = document.querySelectorAll('.dropdown-trigger');
+    var instances = M.Dropdown.init(elems);
+    console.log(instances)
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    for (let guestTable of document.getElementsByClassName("table-dropdown")) {
+        guestTable.addEventListener("click", function () {
+            document.getElementById("chosen-table").innerHTML = this.innerHTML;
+            document.getElementById("table_number").value = this.innerHTML;
+            document.getElementById("chosen-table").style.border = "2px solid #366355";
+        })
+    }
+});
